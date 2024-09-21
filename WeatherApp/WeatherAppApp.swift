5bcaa96fa7f4520f5eb9daf16d8fb57a39f5ad57
Arghadeep Chakraborty <argho.chakr@gmail.com>
2024-09-21 12:23:19 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Arghadeep Chakraborty on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
